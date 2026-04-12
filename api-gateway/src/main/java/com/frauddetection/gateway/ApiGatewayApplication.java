0993d5331bb936ff57ba5cd6967477c5635f6f19
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2026-04-12 16:55:03 -0500

    Add Initial Code for all services

---
+++ api-gateway/src/main/java/com/frauddetection/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.frauddetection.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
